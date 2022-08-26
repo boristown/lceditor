87750c856615169afabf3b05cc2661be91a47728
Boris Town <boristown@gmail.com>
2022-08-26 22:25:42 +0800

    ad sample

--- leetcode.py
+++ leetcode.py
@@ -2,9 +2,19 @@ from typing import *
 
 ###code begin###
 
-
+#1. 两数之和
+#https://leetcode.cn/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
+        return []
 
 ###code end###
 
 if __name__ == '__main__':
-    pass
+    print("123")
