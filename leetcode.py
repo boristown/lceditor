9398505903636265b9d689a0285e7e21a9c56cfe
Boris Town <boristown@gmail.com>
2022-08-26 23:04:11 +0800

    实现输入/执行模块

--- leetcode.py
+++ leetcode.py
@@ -17,4 +17,18 @@ class Solution:
 ###code end###
 
 if __name__ == '__main__':
-    print("123")
+    funcname = dir(Solution)[-1]
+    solu = Solution()
+    func = getattr(solu,funcname)
+    param_cnt = func.__code__.co_argcount-1
+    with open("in.txt","r") as infile:
+        lines = infile.readlines()
+    sample_cnt = len(lines) // param_cnt
+    ind = 0
+    for _ in range(sample_cnt):
+        params = []
+        for _ in range(param_cnt):
+            params.append(eval(lines[ind]))
+            ind += 1
+        ans = func(*params)
+        print(ans)
